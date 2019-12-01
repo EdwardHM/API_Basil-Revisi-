91348ad5074bd7dde70c1645ec375690e157887a
Edward H.M <edwardhm865@gmail.com>
2019-12-01 17:32:41 +0700

    revisi accept data dan access

--- login.php
+++ login.php
@@ -1,15 +1,25 @@
 <?php
+
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: POST");
+header("Access-Control-Max-Age: 3600");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
 require_once 'include/DB_Function.php';
 $db = new DB_Functions();
  
 // json response array
 $response = array("error" => FALSE);
+$data = json_decode(file_get_contents("php://input"));
  
-if (isset($_POST['phone']) && isset($_POST['password'])) {
+if (!is_null($data)) {
  
     // menerima parameter POST ( phone dan password )
-    $phone = $_POST['phone'];
-    $password = $_POST['password'];
+    // $phone = $_POST['phone'];
+    // $password = $_POST['password'];
+    $phone = $data->phone;
+    $password =$data->password;
  
     // get the user by phone and password
     // get user berdasarkan phone dan password
